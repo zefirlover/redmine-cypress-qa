e84fe333ba1c424a20d185e8dde032b5cc7e37fc
zefirlover <ibodikmushta@gmail.com>
2022-05-27 13:44:27 +0300

    Add the forums pages tests

--- cypress/integration/specs/forums.spec.ts
+++ cypress/integration/specs/forums.spec.ts
@@ -1,10 +1,40 @@
 import forumsPage from '../../pages/forums.page'
 
-describe('verify the forums page functionality', function () {
+describe.only('verify the forums page functionality', function () {
     it('Verify the forums page', function () {
         forumsPage.visitMainPage();
         forumsPage.checkForumsTab();
         forumsPage.clickOnForumsTab();
         forumsPage.checkBoardsList();
-    })
+    });
+    it('Verify the Open discussion page', function () {
+        forumsPage.visitForumsPage();
+        forumsPage.checkOpenDiscussionsLink();
+        forumsPage.clickOnOpenDiscussiosLink();
+        forumsPage.displayedElementByText('Open discussion');
+    });
+    it('Verify the Help page', function () {
+        forumsPage.visitForumsPage();
+        forumsPage.checkHelpLink();
+        forumsPage.clickOnHelpLink();
+        forumsPage.displayedElementByText('Help');
+    });
+    it('Verify the Development page', function () {
+        forumsPage.visitForumsPage();
+        forumsPage.checkDevelopmentLink();
+        forumsPage.clickOnDevelopmentLink();
+        forumsPage.displayedElementByText('Development');
+    });
+    it('Verify the Plugins page', function () {
+        forumsPage.visitForumsPage();
+        forumsPage.checkPluginsLink();
+        forumsPage.clickOnPluginsLink();
+        forumsPage.displayedElementByText('Plugins');
+    });
+    it('Verify the Job offers page', function () {
+        forumsPage.visitForumsPage();
+        forumsPage.checkJobOffersLink();
+        forumsPage.clickOnJobOffersLink();
+        forumsPage.displayedElementByText('Job offers');
+    });
 })
--- cypress/pages/forums.page.ts
+++ cypress/pages/forums.page.ts
@@ -9,12 +9,76 @@ class Forums extends Main {
         return cy.get('[class="list boards"]');
     }
 
+    get openDiscussionLink() {
+        return cy.get('a[href="/projects/redmine/boards/1"]');
+    }
+
+    get helpLink() {
+        return cy.get('a[href="/projects/redmine/boards/2"]');
+    }
+
+    get developmentLink() {
+        return cy.get('a[href="/projects/redmine/boards/4"]');
+    }
+
+    get pluginsLink() {
+        return cy.get('a[href="/projects/redmine/boards/3"]');
+    }
+
+    get jobOffersLink() {
+        return cy.get('a[href="/projects/redmine/boards/5"]');
+    }
+
+    visitForumsPage() {
+        cy.visit('projects/redmine/boards');
+    }
+
     checkForumsTab() {
         this.forumsTab.should('be.visible');
     }
 
+    checkOpenDiscussionsLink() {
+        this.openDiscussionLink.should('be.visible');
+    }
+
+    checkHelpLink() {
+        this.helpLink.should('be.visible');
+    }
+
+    checkDevelopmentLink() {
+        this.developmentLink.should('be.visible');
+    }
+
+    checkPluginsLink() {
+        this.pluginsLink.should('be.visible');
+    }
+
+    checkJobOffersLink() {
+        this.jobOffersLink.should('be.visible');
+    }
+
     clickOnForumsTab() {
         this.forumsTab.click();
+    }
+
+    clickOnOpenDiscussiosLink() {
+        this.openDiscussionLink.click();
+    }
+
+    clickOnHelpLink() {
+        this.helpLink.click();
+    }
+
+    clickOnDevelopmentLink() {
+        this.developmentLink.click();
+    }
+
+    clickOnPluginsLink() {
+        this.pluginsLink.click();
+    }
+
+    clickOnJobOffersLink() {
+        this.jobOffersLink.click();
     }
 
     checkBoardsList() {
